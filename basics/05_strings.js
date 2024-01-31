ef1bd60ec645d19a4356673de6fff74bc011eec9
Dollie-git <139562127+Dollie-git@users.noreply.github.com>
2024-01-31 18:44:07 +0000

    strings method

---
+++ basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "dollie";
+const age = 23;
+// console.log(`My name is ${name} and i am ${age} years old.`)
+
+const otherName = new String('Aarti-Mudgal');
+// console.log(otherName[2]);
+// console.log(otherName.toUpperCase());
+// console.log(otherName.length);
+// console.log(otherName.indexOf('i'));
+// console.log(otherName.charAt(3));
+// console.log(otherName.substring(1,4));
+
+// const nextName = otherName.slice(-5,-1)
+// console.log(nextName)
+
+const newName = "  Addie "
+// console.log(newName)
+// console.log(newName.trim())
+
+const url ="https://aadie.com/addie%30mudgal"
+// console.log(url.replace('addie','Addie'))
+// console.log(url.includes('addie'))
+
+console.log(otherName.split('-'))
+
+
+
+
+
